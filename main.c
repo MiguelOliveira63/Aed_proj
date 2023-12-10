45d25b8101ede562dbd41902b1d2671e42599eb7
MiguelOliveira <migueloliveira6333@gmail.com>
2023-12-10 12:17:37 +0000

    criação da função RegistarEmprestimo

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_LIVROS 1000
 #define MAX_ALUNOS 3000
@@ -31,7 +32,7 @@ typedef struct emprestimo{
   int devolvido;
 } Emprestimo;
 
-void registarAluno(Aluno alunos[], *numAlunos){
+void registarAluno(Aluno alunos[], int *numAlunos){
   if(*numAlunos < 3000){
       printf("\nRegistrar novo aluno:\n");
       printf("Matrícula: ");
@@ -49,11 +50,11 @@ void registarAluno(Aluno alunos[], *numAlunos){
     }
 }
 
-void registarLivro(Livro livros[], *numLivros){
+void registarLivro(Livro livros[], int *numLivros){
   if(*numLivros < 1000){
       printf("\nRegistrar novo Livro:\n");
       printf("ISBN: ");
-        scanf("%d", livros[*numLivros].isbn);
+        scanf("%s", livros[*numLivros].isbn);
       printf("Titulo: ");
         scanf("%s", livros[*numLivros].titulo);
       printf("Autores: ");
@@ -68,4 +69,68 @@ void registarLivro(Livro livros[], *numLivros){
         printf("Limite de Livros atingido!!!\n");
     }
 }
+
+void registarEmprestimo(Emprestimo emprestimos[], int *numEmprestimos, Livro livros[], int numLivros[], Aluno alunos[], int numAlunos){
+  char isbn[13];
+  int matricula;
+  int encontrado = 0;
+
+  printf("\nRegistrar novo empréstimo:\n");
+
+  printf("Matricula do Aluno: ");
+  scanf("%d", &matricula);
+
+    //loop para verificar se o aluno existe no sistema
+    for (int i = 0; i < numAlunos; i++) {
+        if (alunos[i].matricula == matricula) {
+            encontrado = 1;
+        }
+    }
+
+    if (!encontrado){
+      printf("O Aluno não existe!\n");
+      return 0;
+    }
+
+    printf("ISBN do Livro para emprestimo: ");
+    scanf("%s", isbn);
+
+    //loop para verificar de o Livro existe no sistema
+    for(int i = 0; i < numLivros; i++){
+      if (strcmp(livros[i].isbn, isbn) == 0) {
+            // Verifica se há exemplares disponíveis
+            if (livros[i].exemplares_disponiveis > 0) {
+
+                // Atribui o valor da matricula do aluno ao emprestimo, atribui o valor armazenado em isbn para o isbn de emprestimo, e da inicio a devolvivo como NAO DEVOLVIDO (0). 
+                emprestimos[*numEmprestimos].matricula = matricula;
+                strcpy(emprestimos[*numEmprestimos].isbn, isbn);
+                emprestimos[*numEmprestimos].devolvido = 0; // Inicializa como não devolvido
+                
+                // Atualiza o numero de livros na posse do aluno e o nnumero de exemplares emprestados
+                for (int j = 0; j < numAlunos; j++) {
+                    if (alunos[j].matricula == matricula) {
+                        alunos[j].livros_na_posse++;
+                    }
+                }
+                livros[i].exemplares_disponiveis--;
+
+                // numTotal de emprestimos
+                (*numEmprestimos)++; 
+                printf("Empréstimo registrado com sucesso.\n");
+            } else {
+                printf("Não há exemplares disponíveis para empréstimo deste livro.\n");
+            }
+            return;
+        }
+    }
+
+    // Se o livro não for encontrado, exibe uma mensagem
+    printf("O Livro Não existe no sistema.\n");
+}
+
+
+    
+
+
+
 
